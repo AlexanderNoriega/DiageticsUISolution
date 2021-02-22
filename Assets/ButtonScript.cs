4a20b1167a4ce0a41cd4cfa623c5a412b41b3582
Alexander Noriega <lordawesomest@gmail.com>
2021-02-22 18:25:08 -0500

    ALl the Stuff

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Text TextField;
+
+    public void setText (string text)
+    {
+        TextField.text = text;
+    }
+}
---
+++ Assets/renderMode.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class renderMode : MonoBehaviour
+{
+    enum RenderModeStates { camera, overlay, world };
+    RenderModeStates m_RenderModeStates;
+
+    Canvas m_Canvas;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Canvas = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Press the space key to switch between render mode states
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeState();
+        }
+    }
+
+    void ChangeState()
+    {
+        switch (m_RenderModeStates)
+        {
+            case RenderModeStates.camera:
+                m_Canvas.renderMode = RenderMode.ScreenSpaceCamera;
+                m_RenderModeStates = RenderModeStates.overlay;
+                break;
+
+            case RenderModeStates.overlay:
+                m_Canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+                m_RenderModeStates = RenderModeStates.world;
+                break;
+            case RenderModeStates.world:
+                m_Canvas.renderMode = RenderMode.WorldSpace;
+                m_RenderModeStates = RenderModeStates.camera;
+
+                break;
+        }
+    }
+}
